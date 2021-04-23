0df884b4c493f4cb1f37bb786b075aaff631aeca
raghav786 <58934695+raghav786@users.noreply.github.com>
2021-04-23 19:27:04 +0530

    third comit

--- src/Header.js
+++ src/Header.js
@@ -3,25 +3,28 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import { useStateValue } from './StateProvider';
 
 const Header = () => {
+
+    const [{ basket }] = useStateValue();
+
+    console.log(basket)
+
     return (
         <div className='header' >
-            {/* logo on the left */}
             <Link to='/'>
                 <img src='https://m.media-amazon.com/images/G/39/gc/designs/livepreview/amazon_dkblue_noto_email_v2016_ae-main._CB441728605_.png' alt='' className='header__logo'  />
             </Link>
             
-            {/* search Box */}
             <div className='header__search'>
                 <input className='header__searchInput' type='text' />
                 <SearchIcon className='header__searchIcon' />
             </div>
             
 
-            {/* 3 links */}
             <div className='header__nav'>
-                {/* 1st Link */}
+
                 <Link to='/login' className='header__link' >
                     <div className='header__option'>
                         <span className='header__optionLineOne' > Hello Raghav </span>
@@ -29,7 +32,7 @@ const Header = () => {
                     </div>
                 </Link>
 
-                {/* 2nd Link */}
+                
                 <Link to='/' className='header__link' >
                     <div className='header__option'>
                         <span className='header__optionLineOne'> Returns </span>
@@ -37,7 +40,7 @@ const Header = () => {
                     </div>
                 </Link>
 
-                {/* 3rd Link */}
+                
                 <Link to='/' className='header__link' >
                     <div className='header__option'>
                         <span className='header__optionLineOne'> Your </span>
@@ -46,14 +49,13 @@ const Header = () => {
                 </Link>
                 
             </div>
-            
-            {/* basket icon with the number */}
+ 
             <Link to='/checkout' className='header__link' >
                 <div className='header__optionBasket' >
                     {/* shopping basket icon */}
                     <ShoppingBasketIcon  />
                     {/* number of items in the basket */}
-                    <span className='header__optionLineTwo  header__basketCount' > 0 </span>
+                    <span className='header__optionLineTwo  header__basketCount' > {basket?.length} </span>
                 </div>
             </Link>
 
--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import './Product.css'
+import { useStateValue } from './StateProvider';
 
 const Product = ({ id, title, price, rating, image  }) => {
+    const [ { basket }, dispatch ] = useStateValue();
+
+    const addToBasket = () => {
+        // add to basket
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating,
+            }
+        })
+    }
+
     return (
         <div className='product' >
             <div className='product__info'>
@@ -20,7 +37,7 @@ const Product = ({ id, title, price, rating, image  }) => {
             </div>
             
             <img src={image} alt='' />
-            <button>
+            <button onClick={addToBasket} >
                 Add to basket
             </button>
         </div>
